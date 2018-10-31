f6bd6de38f594b128154e679fc12d355fe4e1fbb
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2018-10-31 11:57:37 -0600

    Actualizado

--- uno.js
+++ uno.js
@@ -1,3 +1,29 @@
 #######################################
+$("#caja").click(function() {
+    $('#caja').css({ "background": "red", "width": "400px" });
+
+});#######################################
+
+
+#######################################
+Seleccion THIS son dos cajas
+
+$(".cajas").click(function() {
+
+    $(this).css({ "color": "white" });
+    //es decir a la que le doy click
+});
+
+
+#######################################
+
+
+#######################################
+Seleccion por Atributo una caja de texto y un boton
+
+$("#boton").click(function() {
+    $("[type='text']").css("backgorund": "red"); //al campo tipo text
+
+});
 
 #######################################
